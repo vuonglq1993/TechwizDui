c1f67dfd05845a3c83d0fe211418f25ef62be410
Huy Dopin <95194108+Pindos22@users.noreply.github.com>
2024-09-20 09:01:38 +0700

    1

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -110,6 +110,76 @@ const About = () => {
                             />
                         </Col>
                     </Row>
+                    <Row className="mt-4 mb-4 align-items-center">
+                        <Col md="12">
+                            <h3 className="h2 fw-bold">Our Teams</h3>
+                            <p className="mb-2 body-text">
+                                Lorem ipsum dolor sit amet
+                            </p>
+                        </Col>
+                        <Row>
+                            <Col>
+                                <img
+                                    src={group113}
+                                    alt="About Travel Smart"
+                                    className="img-fluid mb-3"
+                                    style={{ maxWidth: "100%" }}
+                                />
+                                <h3 className="h4">Best price guarantee</h3>
+                                <p className="mb-2 body-text">
+                                    Lorem ipsum dolor sit amet
+                                </p>
+                            </Col>
+                            <Col>
+                                <img
+                                    src={group113}
+                                    alt="About Travel Smart"
+                                    className="img-fluid mb-3"
+                                    style={{ maxWidth: "100%" }}
+                                />
+                                <h3 className="h4">Best price guarantee</h3>
+                                <p className="mb-2 body-text">
+                                    Lorem ipsum dolor sit amet
+                                </p>
+                            </Col>
+                            <Col>
+                                <img
+                                    src={group113}
+                                    alt="About Travel Smart"
+                                    className="img-fluid mb-3"
+                                    style={{ maxWidth: "100%" }}
+                                />
+                                <h3 className="h4">Best price guarantee</h3>
+                                <p className="mb-2 body-text">
+                                    Lorem ipsum dolor sit amet
+                                </p>
+                            </Col>
+                            <Col>
+                                <img
+                                    src={group113}
+                                    alt="About Travel Smart"
+                                    className="img-fluid mb-3"
+                                    style={{ maxWidth: "100%" }}
+                                />
+                                <h3 className="h4">Best price guarantee</h3>
+                                <p className="mb-2 body-text">
+                                    Lorem ipsum dolor sit amet
+                                </p>
+                            </Col>
+                            <Col>
+                                <img
+                                    src={group113}
+                                    alt="About Travel Smart"
+                                    className="img-fluid mb-3"
+                                    style={{ maxWidth: "100%" }}
+                                />
+                                <h3 className="h4">Best price guarantee</h3>
+                                <p className="mb-2 body-text">
+                                    Lorem ipsum dolor sit amet
+                                </p>
+                            </Col>
+                        </Row>
+                    </Row>
                     <Row className="mt-4"></Row>
                     <Row className="mt-4">
                         <Col md="3">
@@ -146,118 +216,6 @@ const About = () => {
                         </Col>
                     </Row>
                 </Container>
-
-                {/* <Container>
-                    <Row>
-                        <Col md="8">
-                            <div className="about-content">
-                                <div className="about-image position-relative">
-                                    <img
-                                        src={aboutImg}
-                                        alt="about"
-                                        className="img-fluid rounded-5"
-                                    />
-                                    <div className="about-image-content position-absolute top-50 end-0 p-md-4 p-3 rounded-5 shadow-sm">
-                                        <h3 className="h2 fw-bold text-white">
-                                            HOW WE ARE BEST FOR TRAVEL!
-                                        </h3>
-                                    </div>
-                                </div>
-                            </div>
-                            <h2 className="h2 font-bold pt-4 pb-2">
-                                HOW WE ARE BEST FOR TRAVEL!
-                            </h2>
-                            <p className="body-text mb-2">
-                                Lorem Ipsum is simply dummy text of the printing
-                                and typesetting industry. Lorem Ipsum has been
-                                the industry's standard dummy text ever since
-                                the 1500s, when an unknown printer took a galley
-                                of type and scrambled it to make a type specimen
-                                book.
-                            </p>
-                            <p className="body-text mb-2">
-                                Lorem Ipsum is simply dummy text of the printing
-                                and typesetting industry. Lorem Ipsum has been
-                                the industry's standard dummy text ever since
-                                the 1500s, when an unknown printer took a galley
-                                of type and scrambled it to make a type specimen
-                                book.
-                            </p>
-                            <p className="body-text mb-2">
-                                Lorem Ipsum is simply dummy text of the printing
-                                and typesetting industry. Lorem Ipsum has been
-                                the industry's standard dummy text ever since
-                                the 1500s, when an unknown printer took a galley
-                                of type and scrambled it to make a type specimen
-                                book.
-                            </p>
-                        </Col>
-                        <Col md="4">
-                            <Card className="border-0 shadow-sm rounded-3 mb-4">
-                                <Card.Body className="text-center">
-                                    <div className="d-flex justify-content-center align-items-center my-2">
-                                        <div className="rounded-circle bg-light shadow-sm bg-opacity-10 p-2">
-                                            <img
-                                                src={icons1}
-                                                alt="icon"
-                                                className="img-fluid"
-                                            />
-                                        </div>
-                                    </div>
-                                    <Card.Title className="fw-bold h5">
-                                        50+ Destination
-                                    </Card.Title>
-                                    <p className="mb-2 body-text">
-                                        Lorem Ipsum is simply dummy text of the
-                                        printing and typesetting industry.
-                                    </p>
-                                </Card.Body>
-                            </Card>
-
-                            <Card className="border-0 shadow-sm rounded-3 mb-4">
-                                <Card.Body className="text-center">
-                                    <div className="d-flex justify-content-center align-items-center my-2">
-                                        <div className="rounded-circle bg-light shadow-sm bg-opacity-10 p-2">
-                                            <img
-                                                src={icons2}
-                                                alt="icon"
-                                                className="img-fluid"
-                                            />
-                                        </div>
-                                    </div>
-                                    <Card.Title className="fw-bold h5">
-                                        Best Price In The Industry
-                                    </Card.Title>
-                                    <p className="mb-2 body-text">
-                                        Lorem Ipsum is simply dummy text of the
-                                        printing and typesetting industry.
-                                    </p>
-                                </Card.Body>
-                            </Card>
-
-                            <Card className="border-0 shadow-sm rounded-3 mb-4">
-                                <Card.Body className="text-center">
-                                    <div className="d-flex justify-content-center align-items-center my-2">
-                                        <div className="rounded-circle bg-light shadow-sm bg-opacity-10 p-2">
-                                            <img
-                                                src={icons3}
-                                                alt="icon"
-                                                className="img-fluid"
-                                            />
-                                        </div>
-                                    </div>
-                                    <Card.Title className="fw-bold h5">
-                                        Super Fast Booking
-                                    </Card.Title>
-                                    <p className="mb-2 body-text">
-                                        Lorem Ipsum is simply dummy text of the
-                                        printing and typesetting industry.
-                                    </p>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container> */}
             </section>
         </>
     );
